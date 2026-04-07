ebcce5a989b3fb0b3727dec1ebb7fd2af1737878
Minghui Du <duminghhui@hotmail.com>
2026-04-07 03:52:53 +0200

    dbt tests

--- dbt/climate_transformation/models/intermediate/int_temperature_standardized.sql
+++ dbt/climate_transformation/models/intermediate/int_temperature_standardized.sql
@@ -1,13 +1,27 @@
 with temperature as (
     select * from {{ ref('stg_temperature') }}
 ),
+
 mapping as (
     select * from {{ ref('country_mapping') }}
+),
+
+joined_data as (
+    select
+        m.country_code,
+        t.record_year,
+        t.annual_avg_temp
+    from temperature t
+    inner join mapping m 
+        on t.country = m.temp_name
 )
+
+-- deduplication
 select
-    m.country_code,
-    t.record_year,
-    t.annual_avg_temp
-from temperature t
-inner join mapping m 
-    on t.country = m.temp_name
+    country_code,
+    record_year,
+    -- if multiple original names (like Denmark and Denmark (Europe)) point to the same code,
+    -- we take their average as the temperature for that country and year
+    avg(annual_avg_temp) as annual_avg_temp
+from joined_data
+group by 1, 2
